76bf7cea25407ac977213f950d4ba1fb9c582d02
Krongkaew Ponboobpha <krongkaew@carro.co>
2018-11-28 15:37:49 +0700

    require autoload

--- examples/EchoBot/public/index.php
+++ examples/EchoBot/public/index.php
@@ -20,7 +20,8 @@ use LINE\LINEBot\EchoBot\Dependency;
 use LINE\LINEBot\EchoBot\Route;
 use LINE\LINEBot\EchoBot\Setting;
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require "vendor/autoload.php";
+// require_once __DIR__ . '/../vendor/autoload.php';
 
 $setting = Setting::getSetting();
 $app = new Slim\App($setting);
